55cdf2fdd09036fa523a5e1f79ecd9417453c688
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2024-01-24 21:15:14 +0630

    fix hambuger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import "./navbar.css";
 import { useState } from "react";
 
-import HamburgerIcon from "./base/hamburgerIcon";
+import HamburgerIcon from "../base/HamburgerIcon";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
