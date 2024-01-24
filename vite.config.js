81d6bc4a51ddd1036487db34eb3546eb511b1839
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2024-01-24 22:27:40 +0630

    env variables

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.VITE_MAIL_JS_SERVICE_ID": JSON.stringify(
+      process.env.VITE_MAIL_JS_SERVICE_ID
+    ),
+    "process.env.VITE_MAIL_JS_TEMPLATE_ID": JSON.stringify(
+      process.env.VITE_MAIL_JS_TEMPLATE_ID
+    ),
+    "process.env.VITE_MAIL_JS_PUBLIC_KEY": JSON.stringify(
+      process.env.VITE_MAIL_JS_PUBLIC_KEY
+    ),
+  },
 });
